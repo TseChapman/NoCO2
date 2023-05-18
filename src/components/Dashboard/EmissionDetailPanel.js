307c242f47cb4acf033ff4ce8c1567f634b4da7a
TseChapman <tch222000@gmail.com>
2023-05-18 14:09:37 -0700

    Responsive Panel

--- src/components/Dashboard/EmissionDetailPanel.js
+++ src/components/Dashboard/EmissionDetailPanel.js
@@ -1,14 +1,17 @@
 function EmissionDetailPanel() {
   return (
     <div class="flex justify-center align-middle px-4 mb-4">
-      <div class="bg-limeGreen w-full p-3.5 rounded-2xl flex flex-row">
-        <div class="bg-white w-2/6 h-20 rounded-2xl p-2 shadow-inner shadow-limeGreen flex flex-row justify-between items-center text-3xl mr-8">
+      <div class="bg-limeGreen w-full p-3.5 rounded-2xl flex lg:flex-row flex-col">
+        <div class="bg-white lg:w-2/6 w-full h-20 rounded-2xl p-2 shadow-inner lg:mb-0 mb-3 shadow-limeGreen flex flex-row justify-between items-center lg:text-3xl md:text-2xl text-xl mr-8">
           <div>Goal Emission:</div><div>XXX lb</div>
         </div>
-        <div class="bg-white w-3/6 h-20 rounded-2xl p-2 shadow-inner shadow-limeGreen flex flex-row justify-between items-center text-3xl mr-4">
-          <div>Current Emission:</div><div>XXX lb</div>
+        <div class="lg:w-4/6 w-full flex flex-row">
+          <div class="bg-white w-4/6 h-20 rounded-2xl p-2 shadow-inner shadow-limeGreen flex flex-row justify-between items-center lg:text-3xl md:text-2xl text-xl mr-4">
+            <div>Current Emission:</div><div>XXX lb</div>
+          </div>
+          <button class="bg-cloudy w-2/6 h-20 rounded-2xl p-2 shadow-lg lg:text-4xl md:text-3xl text-2xl font-bold text-merino">Edit</button>
         </div>
-        <button class="bg-cloudy w-1/6 h-20 rounded-2xl p-2 shadow-lg text-4xl font-bold text-merino">Edit</button>
+
       </div>
     </div>
   );
