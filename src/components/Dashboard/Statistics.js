075fd2b4b0b56d83278f476188744f456870968a
TseChapman <tch222000@gmail.com>
2023-05-18 21:26:27 -0700

    Add base Statistics Component

---
+++ src/components/Dashboard/Statistics.js
@@ -0,0 +1,17 @@
+function Statistics() {
+  const statistic = [
+    {statistic: "Highest Emission Activity", topic: "Transport", stat: "XX%"},
+    {statistic: "Average Emission", topic: "XXX days", stat: "XXX lb"},
+    {statistic: "Emission Difference", topic: "Previous day", stat: "- XXX lb"}
+  ]
+
+  return (
+    <div>
+      <div>
+        Statistics
+      </div>
+    </div>
+  );
+}
+
+export default Statistics;
