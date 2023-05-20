182649b3e68cfa6622a52bb83b269be535e16335
TseChapman <tch222000@gmail.com>
2023-05-20 10:15:26 -0700

    Implement Responsive cards

--- src/components/Dashboard/Statistics.js
+++ src/components/Dashboard/Statistics.js
@@ -10,11 +10,13 @@ function Statistics() {
       <div class="bg-matrix w-full p-3.5 rounded-2xl flex flex-col md:flex-row justify-between md:flex-wrap lg:overflow-hidden">
         {statistic.map((card, idx) => {
           return (
-            <div class="h-56 w-full md:w-1/2 lg:w-1/3 p-2">
-              <div class="w-full h-full bg-white rounded-2xl p-3.5 shadow-2xl">
-                <div>{card.statistic}</div>
-                <div>{card.topic}</div>
-                <div>{card.stat}</div>
+            <div class="h-72 w-full md:w-1/2 lg:w-1/3 p-2">
+              <div class="w-full h-full bg-black rounded-2xl p-3.5 shadow-2xl flex flex-col justify-between">
+                <div>
+                  <div class="text-merino text-2xl">{card.statistic}</div>
+                  <div class="text-merino font-bold text-5xl">{card.topic}</div>
+                </div>
+                <div class="text-merino font-bold text-5xl self-end">{card.stat}</div>
               </div>
             </div>
           );
