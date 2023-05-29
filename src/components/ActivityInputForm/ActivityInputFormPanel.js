3f19a637ef7a64b4edc13be78ebf68c2a5b877e4
TseChapman <tch222000@gmail.com>
2023-05-29 15:34:05 -0700

    Implement Transport Panel Logic

--- src/components/ActivityInputForm/ActivityInputFormPanel.js
+++ src/components/ActivityInputForm/ActivityInputFormPanel.js
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import TransportFormPanel from "./TransportFormPanel";
 
 function ActivityInputFormPanel() {
+  const [activities, setActivities] = useState({
+    Transports:[],
+    Foods:[],
+    Utilities:[]
+  })
+
   return (
     <div>
       <div class="px-4">
-        <TransportFormPanel />
+        <TransportFormPanel activities={activities} setActivities={setActivities}/>
       </div>
     </div>
   );
