766e2e449177bfb1d007480a070002a89b3f9603
TseChapman <tch222000@gmail.com>
2023-05-30 18:22:37 -0700

    Add submit activities button event and add navbar buttons

--- src/components/ActivityInputForm/ActivityInputFormPanel.js
+++ src/components/ActivityInputForm/ActivityInputFormPanel.js
@@ -10,14 +10,22 @@ function ActivityInputFormPanel() {
     Utilities:[]
   })
 
+  const handleSubmitActivities = () => {
+
+  };
+
   return (
     <div>
       <div class="px-4">
         <TransportFormPanel activities={activities} setActivities={setActivities}/>
         <FoodFormPanel activities={activities} setActivities={setActivities} />
         <UtilityFormPanel activities={activities} setActivities={setActivities} />
-        <div>
-          <button>Submit Activities</button>
+        <div class="w-full h-24 mb-4">
+          <button
+            class="w-full h-full bg-limeGreen rounded-2xl shadow-sm shadow-gray-700 hover:shadow-md hover:shadow-gray-700 text-merino md:text-6xl text-5xl"
+          >
+            Submit Activities
+          </button>
         </div>
       </div>
     </div>
