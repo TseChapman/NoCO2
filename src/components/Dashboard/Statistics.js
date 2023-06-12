fbaca253adab170042588fcb8d30051a0234ebff
TseChapman <tch222000@gmail.com>
2023-06-12 15:47:58 -0700

    Add animation to statistic card

--- src/components/Dashboard/Statistics.js
+++ src/components/Dashboard/Statistics.js
@@ -1,9 +1,16 @@
+import { useState } from "react";
+
 function Statistics() {
   const statistic = [
     {statistic: "Highest Emission Activity", topic: "Transport", stat: "XX%"},
     {statistic: "Average Emission", topic: "XXX days", stat: "XXX lb"},
     {statistic: "Emission Difference", topic: "Previous day", stat: "- XXX lb"}
   ]
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  function handleLoaded() {
+    setIsLoaded(true);
+  }
 
   const animationDelay = 0.2;
   const animationDuration = 1;
@@ -14,7 +21,11 @@ function Statistics() {
         {statistic.map((card, idx) => {
           return (
             <div class="h-72 w-full md:w-1/2 lg:w-1/3 p-2">
-              <div class="w-full h-full bg-black rounded-2xl p-3.5 shadow-md shadow-gray-700 hover:shadow-lg hover:shadow-gray-700 flex flex-col justify-between">
+              <div
+                class="w-full h-full bg-black rounded-2xl p-3.5 shadow-md shadow-gray-700 hover:shadow-lg hover:shadow-gray-700 flex flex-col justify-between animate-slide-left"
+                style={{animationDelay: `${1 + animationDelay * idx}s`, animationDuration: `${animationDuration}s`, opacity: `${isLoaded ? '100' : '0'}`}}
+                onLoad={handleLoaded}
+              >
                 <div>
                   <div class="text-merino text-2xl">{card.statistic}</div>
                   <div class="text-merino font-bold text-5xl">{card.topic}</div>
