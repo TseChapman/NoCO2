f4ec8e343f837a32e077b7a7e2f70538e9a5a9ad
TseChapman <tch222000@gmail.com>
2023-07-09 16:48:41 -0700

    Modify Dashboard to function component

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react';
 import '../../index.css';
 import LineGraph from './LineGraph';
 import EmissionDetailPanel from './EmissionDetailPanel';
 import Statistics from './Statistics';
 
-export class Dashboard extends Component {
-  render() {
-    return (
-      <div>
-        <div class="text-7xl text-black/0 mb-3">
-          empty
-        </div>
-        <div>
-          <LineGraph />
-          <EmissionDetailPanel />
-          <Statistics />
-        </div>
+function Dashboard() {
+  return (
+    <div>
+      <div class="text-7xl text-black/0 mb-3">
+        empty
       </div>
-    );
-  }
+      <div>
+        <LineGraph />
+        <EmissionDetailPanel />
+        <Statistics />
+      </div>
+    </div>
+  );
 }
+
+export default Dashboard;
