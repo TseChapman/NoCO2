76cf330645886f1b82c1ede7f442f40f9fb9cb12
TseChapman <tch222000@gmail.com>
2023-07-11 22:25:22 -0700

    Integrate submitUserActivities api function to ActivityInputFormPanel component

--- src/components/ActivityInputForm/ActivityInputFormPanel.js
+++ src/components/ActivityInputForm/ActivityInputFormPanel.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { getAuth } from "firebase/auth";
+import { submitUserActivities } from "../../api/NoCO2_api";
 import TransportFormPanel from "./TransportFormPanel";
 import FoodFormPanel from "./FoodFormPanel";
 import UtilityFormPanel from "./UtilityFormPanel";
@@ -10,8 +12,15 @@ function ActivityInputFormPanel() {
     Utilities:[]
   })
 
-  const handleSubmitActivities = () => {
-
+  const handleSubmitActivities = async () => {
+    try {
+      //console.log(activities);
+      const auth = getAuth();
+      const uid = auth.currentUser ? auth.currentUser.uid : null;
+      await submitUserActivities(uid, activities);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
@@ -23,6 +32,7 @@ function ActivityInputFormPanel() {
         <div class="w-full h-24 mb-4">
           <button
             class="w-full h-full bg-limeGreen rounded-2xl shadow-sm shadow-gray-700 hover:shadow-md hover:shadow-gray-700 text-merino md:text-6xl text-5xl"
+            onClick={handleSubmitActivities}
           >
             Submit Activities
           </button>
