34403677c55de29d7396230072baf0dede1948b1
TseChapman <tch222000@gmail.com>
2023-07-13 20:10:33 -0700

    Add base About component

---
+++ src/components/Introduction/About.js
@@ -0,0 +1,22 @@
+function About() {
+  return (
+    <div class="mt-16">
+      <div>
+        <div>no co2 image</div>
+        <div>about text</div>
+      </div>
+      <div>
+        <div>
+          <div>ctse image</div>
+          <div>ctse about</div>
+        </div>
+        <div>
+          <div>luc image</div>
+          <div>luc about</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
