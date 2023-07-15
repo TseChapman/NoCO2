c418dbf8f15802d229464e5a7b59d14a056f0c12
TseChapman <tch222000@gmail.com>
2023-07-14 20:22:02 -0700

    Add base logo image and about project texts

--- src/components/Introduction/About.js
+++ src/components/Introduction/About.js
@@ -1,18 +1,35 @@
+import backgroundImage from '../../assets/BackgroundImage.jpg';
+
 function About() {
+
+  const icon = require('../../assets/noco2-logo.png');
   return (
-    <div class="mt-16">
-      <div>
-        <div>no co2 image</div>
-        <div>about text</div>
+    <div className="w-full h-screen bg-cover bg-no-repeat" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div class="text-7xl text-black/0 mb-3">
+        empty
       </div>
-      <div>
-        <div>
-          <div>ctse image</div>
-          <div>ctse about</div>
+      <div class="px-4">
+        <div className="w-full h-48 flex flex-row rounded-xl overflow-hidden">
+          <div className="w-2/5 relative bg-white">
+            <div className="absolute inset-0 shadow-inner shadow-gray"></div>
+            <img className="w-full h-full object-contain" src={icon} alt="Icon" />
+          </div>
+          <div className="w-3/5 p-2 bg-cloudy">
+            <span className="text-white text-2xl">
+              NoCO2 is a React app that allow user to monitor their CO2 emission.
+              The goal of this project is to help user keep an eye out on their daily CO2 emission to help the United States reach net-zero CO2 emission in 2050.
+            </span>
+          </div>
         </div>
         <div>
-          <div>luc image</div>
-          <div>luc about</div>
+          <div>
+            <div>ctse image</div>
+            <div>ctse about</div>
+          </div>
+          <div>
+            <div>luc image</div>
+            <div>luc about</div>
+          </div>
         </div>
       </div>
     </div>
