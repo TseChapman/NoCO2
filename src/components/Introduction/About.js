688564e510a5528271fc56f0923e8b21d3d4834b
TseChapman <tch222000@gmail.com>
2023-07-18 18:00:23 -0700

    Add Luc About section

--- src/components/Introduction/About.js
+++ src/components/Introduction/About.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import backgroundImage from '../../assets/AboutBg.jpg';
 import aboutCtse from  '../../assets/About/Aboutctse.jpg';
+import aboutLuc from '../../assets/About/Aboutluc.jpg';
 import githubIcon from '../../assets/About/Github_black.png';
 import linkedInIcon from '../../assets/About/LinkedIN.png';
 
@@ -87,8 +88,13 @@ function About() {
               }}
               onLoad={handleLoaded}
             >
-              <div>luc image</div>
-              <div>luc about</div>
+              <img class="h-auto w-4/5 rounded-full" src={aboutLuc} alt='Cheuk Hang Tse img' />
+              <div class="text-3xl">Luc Debaupte</div>
+              <div class="flex flex-row w-full justify-center items-center">
+                <div class="text-xl mr-2">Full-stack Developer</div>
+                <a class="p-1" href="https://github.com/luc779"><img class="w-10 h-10 rounded-xl hover:bg-slate-300" src={githubIcon} alt='Cheuk Hang Tse Github'/></a>
+                <a class="p-1" href="https://www.linkedin.com/in/luc-debaupte/"><img class="w-10 h-10 rounded-xl hover:bg-slate-300" src={linkedInIcon} alt='Cheuk Hang Tse LinkedIn'/></a>
+              </div>
             </div>
           </div>
         </div>
