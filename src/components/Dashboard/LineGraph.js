1f2646635e306e9343478d699a4c14e30a2dd75e
TseChapman <tch222000@gmail.com>
2023-07-29 17:14:12 -0700

    Modify time period height for mobile in LineGraph

--- src/components/Dashboard/LineGraph.js
+++ src/components/Dashboard/LineGraph.js
@@ -68,7 +68,7 @@ function LineGraph({emissionHistory}) {
           <div class="flex h-full">
             {tabsData.map((tab, idx) => {
               const isLastButton = idx === tabsData.length - 1;
-              const buttonClasses = `z-10 h-10 px-2 mt-auto mb-auto flex items-center hover:underline hover:underline-offset-2 lg:text-xl md:text-lg text-md text-merino justify-center${isLastButton ? '' : ' relative'}`;
+              const buttonClasses = `z-10 min-h-10 h-14 px-2 mt-auto mb-auto flex items-center hover:underline hover:underline-offset-2 lg:text-xl md:text-lg text-md text-merino justify-center${isLastButton ? '' : ' relative'}`;
 
               return (
                 <button
