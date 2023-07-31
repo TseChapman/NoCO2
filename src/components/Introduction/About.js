6f17ed6cd06a70c8bf96232fbea1ddbf0d65ef56
TseChapman <tch222000@gmail.com>
2023-07-30 23:59:35 -0700

    Style About component to be responsive to mobile

--- src/components/Introduction/About.js
+++ src/components/Introduction/About.js
@@ -42,15 +42,15 @@ function About() {
               </div>
             </div>
             <div className="w-full md:w-3/5 p-2 bg-cloudy">
-              <span className="text-white text-2xl">
+              <span className="text-white sm:text-2xl text-lg">
                 NoCO2 is a React app that allows users to monitor their CO2 emissions.
                 The goal of this project is to help users keep an eye on their daily CO2 emissions and contribute to achieving the United States' net-zero CO2 emissions target by 2050.
               </span>
             </div>
           </div>
           <div class="w-full flex flex-col md:flex-row mt-4 justify-around">
-            <a class="p-1 rounded-xl bg-white my-2 md:my-0 mx-2 flex flex-row justify-center items-center hover:bg-slate-300 text-2xl" href="https://github.com/TseChapman/NoCO2"><img class="w-10 h-10 rounded-xl mr-2" src={githubIcon} alt='Cheuk Hang Tse Github'/>Front-End Repository</a>
-            <a class="p-1 rounded-xl bg-white my-2 md:my-0 mx-2 flex flex-row justify-center items-center hover:bg-slate-300 text-2xl" href="https://github.com/luc779/BackEndFunctions"><img class="w-10 h-10 rounded-xl mr-2" src={githubIcon} alt='Cheuk Hang Tse Github'/>Back-End Repository</a>
+            <a class="p-1 rounded-xl bg-white my-2 md:my-0 mx-2 flex flex-row justify-center items-center hover:bg-slate-300 sm:text-2xl text-lg" href="https://github.com/TseChapman/NoCO2"><img class="sm:w-10 w-5 sm:h-10 h-5 rounded-xl mr-2" src={githubIcon} alt='Cheuk Hang Tse Github'/>Front-End Repository</a>
+            <a class="p-1 rounded-xl bg-white my-2 md:my-0 mx-2 flex flex-row justify-center items-center hover:bg-slate-300 sm:text-2xl text-lg" href="https://github.com/luc779/BackEndFunctions"><img class="sm:w-10 w-5 sm:h-10 h-5 rounded-xl mr-2" src={githubIcon} alt='Cheuk Hang Tse Github'/>Back-End Repository</a>
           </div>
         </div>
         <div
@@ -60,11 +60,11 @@ function About() {
           }}
           onLoad={handleLoaded}
         >
-          <div class="w-full font-bold text-merino text-6xl flex justify-center">Developers</div>
+          <div class="w-full font-bold text-merino sm:text-6xl text-4xl flex justify-center">Developers</div>
           <hr class="z-10 w-full mt-4 border-merino" />
           <div class="flex flex-col md:flex-row justify-center md:justify-around">
             <div
-              class="flex flex-col bg-merino rounded-xl w-80 md:w-2/5 p-2 justify-center items-center mx-auto my-4 md:m-4 animate-slide-left"
+              class="flex flex-col bg-merino rounded-xl sm:w-80 md:w-2/5 p-2 justify-center items-center mx-auto my-4 md:m-4 animate-slide-left"
               style={{
                 animationDelay: `${1}s`,
                 animationDuration: `${animationDuration}s`,
@@ -73,15 +73,15 @@ function About() {
               onLoad={handleLoaded}
             >
               <img class="h-auto w-4/5 rounded-full" src={aboutCtse} alt='Cheuk Hang Tse img' />
-              <div class="text-3xl">Cheuk Hang Tse</div>
+              <div class="sm:text-3xl text-xl">Cheuk Hang Tse</div>
               <div class="flex flex-row w-full justify-center items-center">
-                <div class="text-xl mr-2">Full-stack Developer</div>
-                <a class="p-1" href="https://github.com/TseChapman"><img class="w-10 h-10 rounded-xl hover:bg-slate-300" src={githubIcon} alt='Cheuk Hang Tse Github'/></a>
-                <a class="p-1" href="https://www.linkedin.com/in/cheuk-hang-chapman-tse/"><img class="w-10 h-10 rounded-xl hover:bg-slate-300" src={linkedInIcon} alt='Cheuk Hang Tse LinkedIn'/></a>
+                <div class="sm:text-xl mr-2">Full-stack Developer</div>
+                <a class="p-1" href="https://github.com/TseChapman"><img class="sm:w-10 w-5 sm:h-10 h-5 rounded-xl hover:bg-slate-300" src={githubIcon} alt='Cheuk Hang Tse Github'/></a>
+                <a class="p-1" href="https://www.linkedin.com/in/cheuk-hang-chapman-tse/"><img class="sm:w-10 w-5 sm:h-10 h-5 rounded-xl hover:bg-slate-300" src={linkedInIcon} alt='Cheuk Hang Tse LinkedIn'/></a>
               </div>
             </div>
             <div
-              class="flex flex-col bg-merino rounded-xl w-80 md:w-2/5 p-2 justify-center items-center mx-auto my-4 md:m-4 animate-slide-left"
+              class="flex flex-col bg-merino rounded-xl sm:w-80 md:w-2/5 p-2 justify-center items-center mx-auto my-4 md:m-4 animate-slide-left"
               style={{
                 animationDelay: `${1 + animationDelay}s`,
                 animationDuration: `${animationDuration}s`,
@@ -90,11 +90,11 @@ function About() {
               onLoad={handleLoaded}
             >
               <img class="h-auto w-4/5 rounded-full" src={aboutLuc} alt='Luc Debaupte img' />
-              <div class="text-3xl">Luc Debaupte</div>
+              <div class="sm:text-3xl text-xl">Luc Debaupte</div>
               <div class="flex flex-row w-full justify-center items-center">
-                <div class="text-xl mr-2">Full-stack Developer</div>
-                <a class="p-1" href="https://github.com/luc779"><img class="w-10 h-10 rounded-xl hover:bg-slate-300" src={githubIcon} alt='Luc Debaupte Github'/></a>
-                <a class="p-1" href="https://www.linkedin.com/in/luc-debaupte/"><img class="w-10 h-10 rounded-xl hover:bg-slate-300" src={linkedInIcon} alt='Luc Debaupte LinkedIn'/></a>
+                <div class="sm:text-xl mr-2">Full-stack Developer</div>
+                <a class="p-1" href="https://github.com/luc779"><img class="sm:w-10 w-5 sm:h-10 h-5 rounded-xl hover:bg-slate-300" src={githubIcon} alt='Luc Debaupte Github'/></a>
+                <a class="p-1" href="https://www.linkedin.com/in/luc-debaupte/"><img class="sm:w-10 w-5 sm:h-10 h-5 rounded-xl hover:bg-slate-300" src={linkedInIcon} alt='Luc Debaupte LinkedIn'/></a>
               </div>
             </div>
           </div>
